c513ee26f4ad7b1c93b54516b47b08696f3c1498
smandoki <80867148+smandoki@users.noreply.github.com>
2022-09-03 20:42:12 +0800

    Completed javacsript and game logic

---
+++ README.md
@@ -0,0 +1,8 @@
+Rock-paper-scissors implemented with vanilla js.
+
+Skills demonstrated
+* HTML
+* CSS
+* Javascript
+
+Site Link https://smandoki.github.io/rock-paper-scissors/
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const choice = {
-    'rock': 0,
-    'paper': 1,
-    'scissors': 2,
+    0: 'rock',
+    1: 'paper',
+    2: 'scissors'
 }
+
+const resultParagraph = document.querySelector('p');
 
 let wins = 0;
 let loss = 0;
@@ -29,3 +31,29 @@ function playRound(playerChoice, computerChoice){
     return `Draw! ${choice[playerChoice]} ties ${choice[computerChoice]}`;
 }
 
+const choices = document.querySelectorAll('img');
+
+choices.forEach(choice => {
+    choice.addEventListener('click', (e) => {
+        e.target.classList.add('clicked');
+
+        const playerChoice = Number(e.target.id);
+        const computerChoice = getComputerChoice();
+
+        let result = playRound(playerChoice, computerChoice);
+        result += `\nPlayer: ${wins} | Computer: ${loss}`
+
+        if (wins === 5 || loss === 5) {
+            result = wins === 5 ? `You Win! ${wins}-${loss}` : `Computer Wins ${loss}-${wins}`;
+            result += '\nPress rock, paper, or scissors to play a new game'
+            wins = 0;
+            loss = 0;
+        }
+
+        resultParagraph.innerText = result;
+    });
+
+    choice.addEventListener('transitionend', (e) => {
+        e.target.classList.remove('clicked');
+    });
+});
